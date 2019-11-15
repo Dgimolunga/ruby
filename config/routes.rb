e68c677e522b4951c0451508a49fcf1ecb0820b6
mch <k735michael@gmail.com>
2019-11-15 09:41:19 +0300

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get 'say/hello'
-  get 'say/goodbye'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  scope controller: :say, path: :say do
+    get :hello
+    get :goodbye
+  end
+
+  root to: 'say#hello'
 end
